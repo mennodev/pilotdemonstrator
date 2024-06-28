f2fa1d2d295e20455720dd1a91157f78cce250f1
mennodev <mennodev@hotmail.com>
2024-06-28 23:38:17 +0200

    tooltip splitter

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ def get_pos(lat, lng):
     return lat, lng
 
 def get_gid_from_tooltip(tooltip_info):
-    st.info(tooltip_info, icon="ℹ️")
-    splitter = tooltip_info.split(' ')
-    gid = splitter[0]
+    st.info(str(tooltip_info))
+    splitter = str(tooltip_info).split('<p>')
+    gid = splitter[1]
     return gid
 # Show the page title and description.
 st.set_page_config(page_title="Betuwe grasslands analysis", page_icon="📈")
