ed6081ad78c3600ae4bf71baa321a046d90fb2b8
mennodev <mennodev@hotmail.com>
2024-06-30 23:25:57 +0200

    all greens

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,13 +46,13 @@ def style_function_nop(x):
     """
     Use color column to assign color
     """
-    return {"color": 'light green', "weight":2}
+    return {"color": 'lightgreen', "weight":2}
 
 def style_function_fw(x):
     """
     Use color column to assign color
     """
-    return {"color": 'dark green', "weight":2}
+    return {"color": 'darkgreen', "weight":2}
 
 betuwe,nop,fw = load_AOIs_geojsons()
 
