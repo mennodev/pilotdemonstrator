8b7a1e5d3a5945191c6b7db510989888d65c25c8
mennodev <mennodev@hotmail.com>
2024-06-30 22:57:32 +0200

    delete page config for navbar

---
+++ pages/NOP.py
@@ -0,0 +1,8 @@
+st.title("NOP")
+
+st.header("Pilot demonstrator for the NOP")
+st.write(
+    """With the tabs in the sidebar different options to visualize the grassland monitor are given.
+    One option is to graph the NDVI over time of the grassland plots. The second option is to plot the NDVI on a map per date.
+    """
+)
