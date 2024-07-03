c1e60accd52e77bc5f3e6b80ce7743a0af2578de
mennodev <mennodev@hotmail.com>
2024-07-03 09:48:11 +0200

    test png image

--- pages/Betuwe.py
+++ pages/Betuwe.py
@@ -120,9 +120,9 @@ m1 = folium.Map()
 #betuwe = gpd.read_file("data/vectors/AOI_Betuwe.geojson")
 #m1.add_geojson('data/vectors/AOI_Betuwe.geojson', layer_name="Betuwe", style=style)
 #raster_path = 'data/rasters/2022-12-23_clipped.tif'
-url_image = 'data/rasters/cloudliness_betuwe.png'
+url_image = 'data/rasters/pf_test1.png'
 #image_bounds = [[-20.664910, -46.538223], [-20.660001, -46.532977]]
-image_bounds = [[51.8516565150000019,5.2416696829999996],[51.9855054920000015,5.8974398399999997]]
+image_bounds = [[51.9221606766716377,5.3395599392786099],[51.9722283239349565,5.4360099571665401]]
 folium.raster_layers.ImageOverlay(
     image=url_image,
     name="image overlay",
