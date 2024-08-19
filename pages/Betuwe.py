f48dd61837075c787a5e6ed3d852fc499ea78715
mennodev <mennodev@hotmail.com>
2024-08-19 09:51:43 +0200

    -debug linked graphs

--- pages/Betuwe.py
+++ pages/Betuwe.py
@@ -405,7 +405,7 @@ with st.expander("Toggle linked Sentinel-2 plot",expanded=True):
     if fid_to_plot_tf is not None:
         # subselect data
         df_selection_tf = df_tf.loc[df_tf['fid'] == fid_to_plot_tf]
-        st.dataframe(data=df_selection.head(20))
+        st.dataframe(data=df_selection_tf.head(20))
         # Display line chart
         chart_tf = alt.Chart(df_selection_tf).mark_line(point={
         "filled": False,
@@ -425,7 +425,7 @@ with st.expander("Toggle linked Sentinel-1 plot",expanded=True):
     if fid_to_plot_tf is not None:
         # subselect data
         df_selection_GRD_tf = df_GRD_tf.loc[df_GRD_tf['fid'] == str(fid_to_plot_tf)]
-        #st.dataframe(data=df_GRD_tf.head(20))
+        st.dataframe(data=df_selection_GRD_tf.head(20))
         # Melt the DataFrame to have a long format suitable for Altair
         df_melted_tf = df_selection_GRD_tf.melt(id_vars=['date', 'fid', 'orbit'], value_vars=['VV', 'VH'], var_name='Polarization', value_name='Value')
         #st.dataframe(data=df_melted.head(10))
