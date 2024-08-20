1f28c9c3512953f9d943447fdaf66ecdd6081e94
mennodev <mennodev@hotmail.com>
2024-08-20 12:44:30 +0200

    - values to list

--- pages/Betuwe.py
+++ pages/Betuwe.py
@@ -626,7 +626,7 @@ with st.expander("Toggle linked interpolated fusion product plot",expanded=True)
         dates = pd.to_datetime(date_columns, format='%Y-%m-%d')
         st.write(dates)
         # Extract the columns that contain the dates and NDVI values
-        ndvi_values = df_selection_pf[date_columns].tolist()
+        ndvi_values = df_selection_pf[date_columns].values.tolist()
         # create df for plotting
         df_ndvi_pf = pd.DataFrame({'NDVI': ndvi_values,'date':dates})
         st.dataframe(df_ndvi_pf)
