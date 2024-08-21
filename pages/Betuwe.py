b4de3fd0e139a8bb25230c3bfab1a165c1159202
mennodev <mennodev@hotmail.com>
2024-08-21 12:17:23 +0200

    -add images and static image insert

--- pages/Betuwe.py
+++ pages/Betuwe.py
@@ -770,3 +770,6 @@ map_bs = st_folium(
     layer_control=control
 )
 
+st.write("Browsing through the map and checking both the winter as summer images often is sufficient to check the declaration of bufferstrips and the area between waterbodies and fruit plantations.Below two cases are presented as examples.")
+st.image(["/data/images/CorrectDecl_1.png","/data/images/CorretDecl_2.png"], caption=["Winter image showing correct delineation of bufferstrip and presence of vegetation","Summer image showing correct delineation of bufferstrip and presence of vegetation"])
+st.image(["/data/images/ProbWrongDecl_1.png","/data/images/ProbWrongDecl_2.png"], caption=["Winter image showing incorrect delineation of bufferstrip and no vegetation","Winter image showing incorrect delineation of bufferstrip and no vegetation"])
