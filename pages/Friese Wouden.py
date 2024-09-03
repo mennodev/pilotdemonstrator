9d7b4dbc9491ece4458e1e81ceb29b4e8aa4a6df
mennodev <mennodev@hotmail.com>
2024-09-03 10:39:11 +0200

    - df from list

--- pages/Friese Wouden.py
+++ pages/Friese Wouden.py
@@ -266,7 +266,7 @@ ammount_dict = {"Water carying ditch":"50379",
 "Earth banks and sand walls":"3",
 "Feed hedge":"1"}
 
-df_LE_ammounts = pd.DataFrame.from_dict(ammount_dict)
+df_LE_ammounts = pd.DataFrame.from_dict(list(ammount_dict.items), column=['LF type','Occurence'])
 
 container = st.container(border=True) 
 container.write(f"**Types of agricultural landscape features**")
